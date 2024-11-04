08959a32d986ab959c4eceeba63a172bf13e6dbc
Hermanet Lay <laychinhan@gmail.com>
2024-11-04 21:37:34 +0700

    feat: query for CRUD and READ

---
+++ backend/db/postgresql/query.sql
@@ -0,0 +1,44 @@
+-- name: GetPagedTasks :many
+SELECT id, name, description, due_date
+FROM tasks
+WHERE ((CASE WHEN @unchecked::boolean THEN true ELSE false END) AND (is_done IS NULL))
+ORDER BY CASE
+             WHEN NOT @reverse::boolean AND @order_by::text = 'due_date' THEN due_date
+             END ASC,
+         CASE
+             WHEN NOT @reverse::boolean AND @order_by::text = 'id' THEN id
+             END ASC,
+         CASE
+             WHEN @reverse::boolean AND @order_by::text = 'due_date' THEN due_date
+             END DESC,
+         CASE
+             WHEN @reverse::boolean AND @order_by::text = 'id' THEN id
+             END DESC
+LIMIT $1 OFFSET $2;
+
+-- name: GetPagedTasksByName :many
+SELECT id, name, description, due_date
+FROM tasks
+WHERE name_search @@ to_tsquery('english', $1)
+ORDER BY id
+LIMIT $2 OFFSET $3;
+
+-- name: CreateTask :one
+INSERT INTO tasks (name, description, due_date)
+VALUES($1, $2, $3)
+RETURNING id, name, description, due_date;
+
+-- name: UpdateTask :one
+UPDATE tasks
+SET name        = $1,
+    description = $2,
+    due_date    = $3
+WHERE id = $4
+RETURNING id, name, description, due_date;
+;
+
+-- name: DeleteTask :one
+DELETE
+FROM tasks
+WHERE id = $1
+RETURNING id;
---
+++ backend/db/postgresql/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS tasks
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(300)             NOT NULL,
+    description TEXT                     NOT NULL,
+    name_search TSVECTOR GENERATED ALWAYS AS ( TO_TSVECTOR('english', name) ) STORED,
+    due_date    TIMESTAMP WITH TIME ZONE NOT NULL
+);
